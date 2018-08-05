f108ec9b6ff4ad2ae2b72288720c7e9aaf1a7c01
amylnikov <a.mylnikov@id-sys.ru>
2018-08-05 16:24:00 +0300

    Задание №2 (Часть 1 - Функция distance не ассоциирована с классом Point)

--- sandbox/src/main/java/ru/stqa/prt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/prt/sandbox/MyFirstProgram.java
@@ -3,21 +3,17 @@ package ru.stqa.prt.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("world");
-    hello("User");
-    hello("Alexei");
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    Point p1 = new Point(2, 4);
+    Point p2 = new Point(6, 7);
 
-    Rectangle r = new Rectangle(4,6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    System.out.println("Расстояние между точкой А(" + p1.x + ", " + p1.y + ") и точкой B(" + p2.x + ", " + p2.y + ") составляет " + distance(p1, p2));
 
   }
 
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody + "!");
-  }
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
 
+  }
 
 }

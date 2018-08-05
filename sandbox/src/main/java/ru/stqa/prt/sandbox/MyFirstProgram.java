ee7f9e32bbe1f41ce1bf3c8a4fe4010610491699
amylnikov <a.mylnikov@id-sys.ru>
2018-08-05 16:40:29 +0300

    Задание №2 (Часть 2 - Функция distance ассоциирована с классом Point)

--- sandbox/src/main/java/ru/stqa/prt/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/prt/sandbox/MyFirstProgram.java
@@ -4,16 +4,11 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point(2, 4);
-    Point p2 = new Point(6, 7);
+    Point p = new Point(2, 4, 6,7);
 
-    System.out.println("Расстояние между точкой А(" + p1.x + ", " + p1.y + ") и точкой B(" + p2.x + ", " + p2.y + ") составляет " + distance(p1, p2));
-
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
+    System.out.println("Расстояние между точкой А(" + p.x1 + ", " + p.y1 + ") и точкой B(" + p.x2 + ", " + p.y2 + ") составляет " + p.distance());
 
   }
+
 
 }

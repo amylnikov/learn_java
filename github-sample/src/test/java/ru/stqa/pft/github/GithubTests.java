fa948e5a5199743fac8ef398d08963d970fcb8f0
amylnikov <a.mylnikov@id-sys.ru>
2018-10-04 22:19:10 +0300

    Урок 9.6

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("e1f2945f59839261280cf26796ba69c52932044d");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("amylnikov", "learn_java")).commits();
+    for(RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())){
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
